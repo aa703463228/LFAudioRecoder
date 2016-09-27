e3cecf130cd4448dd03668ff567c4fb0f46c1aef
萧凡宇 <703463228@qq.com>
2016-09-27 17:22:49 +0800

    Initial Commit

---
+++ LFAudioRecoder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LFAudioRecoder
+//
+//  Created by mxc235 on 16/9/27.
+//  Copyright © 2016年 mxc235. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LFAudioRecoder/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LFAudioRecoder
+//
+//  Created by mxc235 on 16/9/27.
+//  Copyright © 2016年 mxc235. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
